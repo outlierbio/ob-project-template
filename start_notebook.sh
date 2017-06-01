a0be8923d7e06c3d59b31ddf74ecaee974b19139
Jake Feala <jake@outlierbio.com>
2017-06-01 11:00:44 -0400

    Mount volume on proper work directory
    Latest updates to r-notebook base image put the home directory in /home/jovyan/work. Mount the top-level directory to this folder in the container.

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,4 +1,4 @@
 # Start Jupyter notebook container
 IMAGE_NAME=outlierbio/$(basename $(pwd))
 docker build -t $IMAGE_NAME .
-docker run --rm -it -p 8888:8888 -v "$(pwd):/notebooks" $IMAGE_NAME
+docker run --rm -p 8888:8888 -v "$(pwd):/home/jovyan/work" $IMAGE_NAME
