dbe45c69eac63632d1bf8bf982189d92f834460b
Jake Feala <jake@outlierbio.com>
2018-01-03 19:53:02 -0500

    Switch Dockerfile inheritance and installs based on platform

---
+++ {{ cookiecutter.project_slug }}/install_packages.r
@@ -0,0 +1,10 @@
+{% if cookiecutter.platform_slug == 'bioconductor' %}
+source('http://bioconductor.org/biocLite.R');
+biocLite(c(
+  'Biobase'
+))
+{% endif %}
+
+install.packages(c(
+  'tidyverse',
+), repos='http://cran.r-project.org')
